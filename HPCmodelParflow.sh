41a8b1d4b04a4c8b8f0657a702782a2cb436e6e3
cvoter <cvoter@wisc.edu>
2018-01-24 16:24:51 -0600

    Created parent script with HPC-specific environment variables. Calls process from inputsCreateTAR to outputsCreatPFB

---
+++ HPCmodelParflow.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Carolyn Voter
+# HPCmodelParflow.sh
+# Establishes HPC environment variables before calling runParflow executable.
+
+# ==============================================================================
+# SLURM REQUESTS
+# ==============================================================================
+#SBATCH --partition=pre
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --error=/home/cvoter/Jobs/%J.err
+#SBATCH --output=/home/cvoter/Jobs/%J.out
+
+# ==============================================================================
+# INTERPRET ARGUMENTS
+# 1 = runname
+# 2 = totalHr = total number of model hours for complete simulation
+# 3 = drun = number of model hours per loop
+# nruns = number of loops to execute, based on total hours and hours per loop
+# ==============================================================================
+export runname=$1
+export totalHr=$2
+export drun=$3
+export np=$4
+export nruns=$(((totalHr+drun-1)/drun))
+
+# ==============================================================================
+# SET ENVIRONMENT VARIABLES
+# Paths for required libraries and relevant directories
+# ==============================================================================
+module load mpi/gcc/mpich-3.1
+export BASE=/home/cvoter/ParFlow
+export PARFLOW_DIR=$BASE/parflow
+export TCL_PATH=$BASE/tcl-8.6.8
+export LD_LIBRARY_PATH=$TCL_PATH/lib:$LD_LIBRARY_PATH
+export HOME=/scratch/local/cvoter
+export GHOME=/home/cvoter/$runname
+export SCRIPTS=/home/cvoter/PFscripts
+
+# ==============================================================================
+# 1. TAR INPUT FILES
+# ==============================================================================
+date +"%H:%M:%S %Y-%m-%d"
+printf "Tarring input files....\n"
+sh $SCRIPTS/inputsCreateTAR.sh
+
+# ==============================================================================
+# 2. CONVERT SA INPUTS TO PFB INPUTS
+# ==============================================================================
+printf "\n\n"
+date +"%H:%M:%S %Y-%m-%d"
+printf "Converting *.sa input files....\n"
+sh $SCRIPTS/inputsConvertSA.sh
+
+# ==============================================================================
+# 3. RUN PARFLOW MODEL
+# ==============================================================================
+printf "\n\n"
+date +"%H:%M:%S %Y-%m-%d"
+printf "Running parflow model files....\n"
+sh $SCRIPTS/runParflow.sh
+
+# ==============================================================================
+# 4. REARRANGE OUTPUTS
+# ==============================================================================
+printf "\n\n"
+date +"%H:%M:%S %Y-%m-%d"
+printf "Rearranging output files....\n"
+sh $SCRIPTS/outputsRearrangeTAR.sh
+
+# ==============================================================================
+# 5. GENERATE OTHER REQUIRED PFB OUTPUTS
+# ==============================================================================
+printf "\n\n"
+date +"%H:%M:%S %Y-%m-%d"
+printf "Generating other required *.pfb output files....\n"
+for storage in subsurface_storage surface_storage; do
+    export flux=$storage
+    sh $SCRIPTS/outputsCreatePFB.sh
+done
+
+# ==============================================================================
+# 6. DONE
+# ==============================================================================
+printf "\n\n"
+date +"%H:%M:%S %Y-%m-%d"
+printf "All done!"
