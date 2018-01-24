f4c6ad61ca73c22cfe0aec267e97ad85f1ae6129
Carolyn Voter <cvoter@wisc.edu>
2018-01-24 15:08:44 -0600

    Added inputsCreateTAR.sh. Takes individual input files and combines into tarballs before running parflow.

---
+++ inputsCreateTAR.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Carolyn Voter
+# tarInputs.sh
+# Takes parflow input files and combines in appropriate tar files
+
+# Usage: sh tarInputs.sh
+# Requires the following environment variables to be defined in parent script:
+# runname - name of modeling run
+# GHOME - path to where inputs and outputs are stored for $runname
+# SCRIPTS - path to where PFscripts are stored
+
+# ==============================================================================
+# DEFINE FUNCTIONS
+# ==============================================================================
+# ------------------------------------------------------------------------------
+# CREATE TARBALL OF SPECIFIED INPUTS
+# ------------------------------------------------------------------------------
+tarInput () { 
+    for inputDir in $@; do
+	    #Create directory
+	    mkdir $inputDir
+	
+	    #Move appropriate inputs to directory
+	    if [ "$inputDir" = "SAin" ]; then
+            mv *.sa $inputDir/
+            cp $SCRIPTS/inputsConvertSA.tcl $inputDir/
+        elif [ "$inputDir" = "PFin" ]; then
+            mv drv_clmin_start.dat drv_clmin_restart.dat drv_vegm.dat drv_vegp.dat nldas.1hr.clm.txt parameters.txt $inputDir/
+            cp $SCRIPTS/runParflow.tcl $inputDir/
+			cp $SCRIPTS/logComments.sh $SCRIPTS/saveCurrentOutputs.sh $inputDir/
+        else
+            mv domainInfo.mat precip.mat $inputDir/
+        fi
+	  
+	    #Tar directory, remove untarred version
+	    tar zcf $inputDir.tar.gz $inputDir
+        rm -rf $inputDir
+    done
+}
+	
+# ==============================================================================
+# CREATE SAin
+# Tarred file of *.sa inputs
+# ==============================================================================
+cd $GHOME
+tarInput SAin PFin MATin
+rm *.fig
